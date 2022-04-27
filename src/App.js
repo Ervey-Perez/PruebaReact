d14782403b0528f4c2918039b819efe518d0fce1
ERVEY PEREZ <erveyperez23@gmail.com>
2022-04-27 12:27:46 -0600

    Primer cambio la rama js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cambio en javaScript
         </a>
       </header>
     </div>
